a080601d3bc0364bc5f403ea926d1c87d26629b9
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-06-28 22:28:16 +0530

    minor ui update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,14 +27,14 @@ function Home() {
         <meta property="og:image" content="URL_to_image" />
       </Helmet>
 
-      <div className="flex flex-col py-6 md:py-5 text-center justify-center items-center w-screen mx-auto mt-10 md:mt-24">
+      <div className="flex flex-col py-6 md:py-auto text-center justify-center items-center w-screen mx-auto mt-10 md:mt-24">
         <div className="justify-center">
           <img
-            className="border-2 border-black filter- rounded-[50%] h-20 w-20 md:h-40 md:w-40 m-auto"
-            src="https://raw.githubusercontent.com/c4coderandcreator/BhaiBhai_LoginPortal/main/img/profile.jpg"
-            alt="profile_img"
+            className="border-2 border-black filter- rounded-[10%] h-20 w-20 md:h-40 md:w-40 m-auto"
+            src="./Anay.png"
+            alt="Anay Singh"
           />
-          <h1 className="text-webtext-400 text-xl md:text-4xl font-monoDM font-bold my-3">
+          <h1 className="text-webtext-400 md:text-center text-xl md:text-4xl font-monoDM font-bold my-8 md:my-12 md:pt-4">
             HEY I'M <span className="text-webbut-400">ANAY</span>
           </h1>
           <h3 className="md:mb-4 rounded-lg text-sm md:text-xl p-2 md:p-4 md:border-2 md:border-webtext-400 font-monoDM text-webtext-400 w-screen md:w-[55vw] md:my-4 mx-auto justify-center text-center md:text-justify">
@@ -44,7 +44,7 @@ function Home() {
             create something remarkable together!
           </h3>
         </div>
-        <div className="text-xs md:text-base">
+        <div className="text-xs md:text-base md:mt-8">
           <button className="font-monoDM font-medium text-lg md:text-2xl border-2 border-webbut-400 bg-webbut-400 hover:bg-black hover:border-webbut-400 hover:border-2 hover:text-webbut-400 md:px-8 px-4 py-1 md:py-2 mr-3 rounded-lg duration-300">
             <Link to="https://drive.google.com/file/d/1VqTGnrBWHw4a0H5gFwTxHvX3IedpWUSN/view?usp=sharing">
               Resume
@@ -82,7 +82,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
